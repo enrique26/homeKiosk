8de0691b0cbd8c1f760baa0698d59e85cbfa5183
Enrique <enriqueibilis@gmail.com>
2018-07-10 16:53:41 -0500

    test launcher setup

--- demo/app/controllers/index.js
+++ demo/app/controllers/index.js
@@ -2,4 +2,10 @@ function doClick(e) {
 	alert($.label.text);
 }
 
+
+function callHome(){
+	Ti.API.info('Button Home');
+	return false;
+}
+$.index.addEventListener('androidhome',callHome);
 $.index.open();
